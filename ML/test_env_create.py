02f279ed69453db76c3c93760b75c9ec082bb46a
C0atRack <--set>
2025-02-22 08:07:38 -0700

    Have the model default to using cuda for accelerated training.

--- ML/test_env_create.py
+++ ML/test_env_create.py
@@ -4,6 +4,6 @@ from GameWrapper.wrappers.DummyWrapper import DummyWrapper
 
 if "__main__" in __name__:
     env = SmwEnvironment(DummyWrapper())
-    model = SAC("MlpPolicy", env, verbose=1, buffer_size=1, device="gpu")
+    model = SAC("MlpPolicy", env, verbose=1, buffer_size=1, device="cuda")
 
     model.learn(total_timesteps=500)
